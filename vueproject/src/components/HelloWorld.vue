7d3886b62d7e1a2265f0cefff1be48215ed2ed46
zhaobaoqiang <zhaobaoqiang88888@126.com>
2020-02-17 10:26:51 +0800

    样式学习

--- vueproject/src/components/HelloWorld.vue
+++ vueproject/src/components/HelloWorld.vue
@@ -1,32 +1,13 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="div1">111</div>
+    <div class="div2">222</div>
+    <div class="div3">333</div>
+    <div class="div4">444</div>
+    <div class="div5">555</div>
+    <p class="serif">这一段的字体是 Times New Roman </p>
+    <p class="sansserif">这一段的字体是 Arial.</p>
+
   </div>
 </template>
 
@@ -41,18 +22,28 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.hello
+{
+  position: absolute;
+  left: 50px;
+  width:calc(100% - 100px);
+  background-color:violet;
+  display: flex;
+  flex-direction: column;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.div1{
+  background-color: blue;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.div5{
+  background-color: blueviolet;
 }
-a {
-  color: #42b983;
+.serif
+{
+  font-family: 'Times New Roman', Times, serif;
+}
+.sansserif
+{
+  font-family: Arial, Helvetica, sans-serif;
+  font-size: 10;
 }
 </style>
